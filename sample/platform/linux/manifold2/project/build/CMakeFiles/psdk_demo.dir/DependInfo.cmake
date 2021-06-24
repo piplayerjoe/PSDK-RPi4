d11347686f6d6e546c5533ff59a70c3f4f219454
JoJoBird <1509777237@qq.com>
2021-06-24 10:47:27 +0800

    1st commit

---
+++ sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/camera_emu/test_payload_cam_emu.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/camera_emu/test_payload_cam_emu.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/camera_media_emu/psdk_media_file_manage/psdk_media_file_core.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/camera_media_emu/psdk_media_file_manage/psdk_media_file_core.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/camera_media_emu/psdk_media_file_manage/psdk_media_file_jpg.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/camera_media_emu/psdk_media_file_manage/psdk_media_file_jpg.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/camera_media_emu/psdk_media_file_manage/psdk_media_file_mp4.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/camera_media_emu/psdk_media_file_manage/psdk_media_file_mp4.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/camera_media_emu/test_payload_cam_media.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/camera_media_emu/test_payload_cam_media.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/data_channel/test_data_channel.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/data_channel/test_data_channel.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/data_subscription/test_data_subscription.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/data_subscription/test_data_subscription.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/data_transmission/test_data_transmission.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/data_transmission/test_data_transmission.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/gimbal_emu/test_payload_gimbal_emu.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/gimbal_emu/test_payload_gimbal_emu.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/mop_channel/test_mop_channel.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/mop_channel/test_mop_channel.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/payload_collaboration/test_payload_collaboration.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/payload_collaboration/test_payload_collaboration.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/positioning/test_positioning.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/positioning/test_positioning.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/power_management/test_power_management.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/power_management/test_power_management.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/time_synchronization/test_time_sync.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/time_synchronization/test_time_sync.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/upgrade/test_upgrade.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/upgrade/test_upgrade.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/upgrade/test_upgrade_common_file_transfer.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/upgrade/test_upgrade_common_file_transfer.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/upgrade/test_upgrade_platform_opt.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/upgrade/test_upgrade_platform_opt.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/utils/util_buffer.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/utils/util_buffer.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/utils/util_file.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/utils/util_file.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/utils/util_math.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/utils/util_math.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/utils/util_md5.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/utils/util_md5.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/utils/util_misc.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/utils/util_misc.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/utils/util_time.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/utils/util_time.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/widget/file_binary_array_list_en.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/widget/file_binary_array_list_en.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/widget/test_widget.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/widget/test_widget.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/xport/test_xport.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/api_sample/xport/test_xport.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/common/monitor/sys_monitor.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/common/monitor/sys_monitor.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/common/osal/osal.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/common/osal/osal.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/common/upgrade_platform_opt/upgrade_platform_opt_linux.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/common/upgrade_platform_opt/upgrade_platform_opt_linux.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/application/main.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/application/main.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/hal/hal_network.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/hal/hal_network.c.o"
+  "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/hal/hal_uart.c" "/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/project/build/CMakeFiles/psdk_demo.dir/home/pi/Payload_SDK_V2.2.1-build.315-raspi/sample/platform/linux/manifold2/hal/hal_uart.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "PSDK_ARCH_SYS_LINUX=1"
+  "_GNU_SOURCE"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../.."
+  "../../../common"
+  "../../../../../api_sample"
+  "../../../../../../psdk_lib/api_headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
